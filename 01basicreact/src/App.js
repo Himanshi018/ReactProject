d93613ec101c108b1bd4ef8f83c7e7c41acf8a0b
Himanshi018 <himanshijigenius2003@gmail.com>
2024-02-25 20:15:40 +0530

    01 Basic React Access

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+import Chai from "./Chai"
+
+function App() {
+  return (
+    <>
+    <Chai />
+    <h1>New react | Hi I am Himanshi. Today I have started learning react</h1>
+    </>
+  );
+}
+
+export default App;
